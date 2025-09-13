71292c1c8c1338e0754e71fec28d85cdf5210ee3
feliperj629 <feliperj629@gmail.com>
2025-09-12 21:43:35 -0300

    Incluindo novas funçoes.

--- App.js
+++ App.js
@@ -1,30 +1,68 @@
 import { useState } from "react";
-import { Text, View, TextInput, Button } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   const [nome, setNome] = useState("");
   const [mensagem, setMensagem] = useState("");
+  const [contar, setContar] = useState(0);
+  const [cor, setCor] = useState("");
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 20 }}>
+    <View style={[styles.container, { backgroundColor: cor ? cor : "" }]}>
       <Text style={{ fontSize: 24, marginBottom: 20 }}>Bem-vindo ao appExercicio1!</Text>
 
       <TextInput
         placeholder="Digite seu nome"
         value={nome}
         onChangeText={setNome}
-        style={{
-          borderWidth: 1,
-          borderColor: "#ccc",
-          padding: 10,
-          width: "80%",
-          marginBottom: 20,
-        }}
+        style={styles.input}
       />
 
-      <Button title="Mostrar mensagem" onPress={() => setMensagem(`Olá, ${nome}!`)} />
+      <Button title="Mostrar mensagem"
+        onPress={() => {
+          setMensagem(`Olá, ${nome}!`);
+          setContar(contar + 1);
+        }
+        }
+      />
 
-      {mensagem ? <Text style={{ marginTop: 20, fontSize: 18 }}>{mensagem}</Text> : null}
+      {mensagem ? <Text style={styles.text}>{mensagem} {contar}</Text> : null}
+
+
+      <Button title="Limpar"
+        onPress={() => { setNome(""); setMensagem(""); }} />
+
+      <TextInput
+        placeholder="Digite uma cor:"
+        value={cor}
+        onChangeText={setCor}
+        style={styles.input}
+      />
+
+      {cor ? <Text style={styles.text}>{cor} </Text> : null}
+
+
     </View>
   );
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    width: "80%",
+    marginBottom: 20,
+  },
+  text: {
+    marginTop: 20,
+    fontSize: 18
+  },
+});
